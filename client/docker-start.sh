2087bbf89d4e66054e1e0de2ea7533ff599984ff
Lugrim <Lugrim@users.noreply.github.com>
2024-03-03 16:51:35 +0100

    🔧 Changed ports configuration
    - The frontend docker will listen on `PORT` or 80 even inside the
      docker. It allows to run the client standalone.
    - The frontend docker can now can take a `BACKEND_PORT` environment
      variable

--- client/docker-start.sh
+++ client/docker-start.sh
@@ -3,6 +3,10 @@
 # Integrate environment variables
 sed -i "s|__BACKEND__|${BACKEND_HOST}|" \
   /etc/nginx/nginx.conf
+sed -i "s|__LISTEN_PORT__|${PORT:-80}|" \
+  /etc/nginx/nginx.conf
+sed -i "s|__BACKEND_PORT__|${BACKEND_PORT:-6666}|" \
+  /etc/nginx/nginx.conf
 sed -i "s|__BASEURL__|${BASE_URL:-/}|g" \
   /var/www/index.htm \
   /var/www/manifest.json
